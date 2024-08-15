51bf83540db8a54fca47d3c42beede4cd1447d20
santoshkumarm1 <santoshkumarm1@verifone.com>
2024-08-15 07:44:47 +0530

    devops kotlin setup.

---
+++ src/main/kotlin/com/devops/devopskotlin/DevopskotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.devops.devopskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevopskotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevopskotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/devops/devopskotlin/DevopskotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devops.devopskotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevopskotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
